4c9d64f6fb80a25f9b0ab4ecf6a763cb6064ab1f
Jose Antonio Marin Lopez <joseantonio.marinlopez@xploregroup.es>
2021-09-23 16:43:55 +0200

    asdads

--- projects/xplorecomponents/src/lib/components/geolocation/test-geolocation-blocking.module.ts
+++ projects/xplorecomponents/src/lib/components/geolocation/test-geolocation-blocking.module.ts
@@ -1,6 +1,6 @@
 import { CommonModule } from "@angular/common";
 import { NgModule } from "@angular/core";
-import { LanguageService, provideConfig } from "@spartacus/core";
+import { CmsConfig, LanguageService, provideConfig } from "@spartacus/core";
 import { TestGeolocationBlockingComponent } from "./test-geolocation-blocking";
 import { translationChunksConfig } from '@spartacus/assets';
 import {
@@ -32,7 +32,7 @@ import { NgbModule } from "@ng-bootstrap/ng-bootstrap";
           chunks: translationChunksConfig,
           fallbackLang: 'en',
         },
-      }),
+      } as CmsConfig),
     ],
 })
 
